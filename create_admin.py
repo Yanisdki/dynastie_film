7fb5bfb0b5f28efb32c17029e5caf1b93ca07fda
Douki Yanis <doukiyanis@gmail.com>
2026-06-01 20:37:04 +0200

    ajout du script creation admin

---
+++ create_admin.py
@@ -0,0 +1,19 @@
+import os
+import django
+
+# Configure l'environnement Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aesthetic_portfolio.settings')
+django.setup()
+
+from django.contrib.auth.models import User
+
+# Remplace par les identifiants que tu souhaites
+USERNAME = 'yanis'
+EMAIL = 'doukiyanis@gmail.com'
+PASSWORD = 'TonMotDePasseSecurise Ici'  # Mets un vrai mot de passe complet
+
+if not User.objects.filter(username=USERNAME).exists():
+    User.objects.create_superuser(USERNAME, EMAIL, PASSWORD)
+    print("🚀 Superuser créé avec succès sur Render !")
+else:
+    print("ℹ️ Le superuser existe déjà.")
